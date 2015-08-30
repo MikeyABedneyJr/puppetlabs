976d2c3edca1024ae1c0c7e25e6f34f001a6007c
Mike (Mikey) Bedney <mike.bedney@gmail.com>
2015-08-30 13:02:34 -0700

    Update
    The two files needed to complete a tech challenge

---
+++ main.pp
@@ -0,0 +1,33 @@
+# Ensures puppetlabs-ngnix module is installed
+package { 'puppetlabs-nginx':
+  ensure => present,
+}
+
+# Check that the directory is present before fetching exercise repo
+file { '/var/www':
+  ensure  => directory,
+}
+
+# Clone repo
+exec { 'git clone https://github.com/puppetlabs/exercise-webpage':
+  cwd     => '/var/www',
+  creates => '/var/www/exercise-webpage/index.html',
+  path    => '/usr/bin',
+}
+
+# Ensures nginx is running
+service { 'nginx':
+  enable => true,
+  ensure => running,
+}
+
+# Turn off default nginx.conf so later we can implement custom settings
+file { '/etc/nginx/sites-enabled/default':
+  ensure => absent,
+}
+
+# Deploy custom nginx config to puppet agent(s)
+file { "/etc/nginx/nginx.conf":
+  ensure => present,
+  source =>"puppet:///nginx.conf"
+}
